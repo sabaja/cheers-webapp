868b2ce482311677b3c56e0a4bbfb77e6b345e8c
sabaja <jacoposabatini76@gmail.com>
2020-10-16 16:37:06 +0200

    START project BE

---
+++ src/main/java/com/cheers/CheersWebappApplication.java
@@ -0,0 +1,13 @@
+package com.cheers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheersWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheersWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheers/CheersWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheersWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
